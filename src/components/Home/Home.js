e2652caf3614f3dc0e51ed34b8a63e9a86e1db75
Matt Gill <mtgill90@outlook.com>
2019-07-06 03:07:17 -0500

    setup, auth, and basuis routing working

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Home.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>Home</h2>
+    </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/MyNavBar/MyNavBar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { NavLink as RRNavLink } from 'react-router-dom';
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './MyNavBar.scss';
+
+class MyNavBar extends React.Component {
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
+  state = {
+    isOpen: false,
+  }
+
+  toggle() {
+    this.setState({ isOpen: !this.isOpen });
+  }
+
+  render() {
+    const { authed } = this.props;
+    const buildNavBar = () => {
+      if (authed) {
+        return (
+          <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/home">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/new">New Stuff</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={RRNavLink} to="/myStuff">My Stuff</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink onClick={this.logMeOut}>Logout</NavLink>
+          </NavItem>
+        </Nav>
+        );
+      }
+      return <Nav className="ml-auto" navbar />;
+    };
+    return (
+  <div className="MyNavBar">
+<Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Hoarders</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            {buildNavBar()}
+          </Collapse>
+        </Navbar>
+  </div>
+    );
+  }
+}
+
+export default MyNavBar;
